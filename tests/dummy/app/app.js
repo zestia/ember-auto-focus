d70b80276107aab1718a83cd3e352f7b949c61f5
Andrew Kirwin <andrew.kirwin@gmail.com>
2017-05-18 13:56:46 +0100

    add more eslint

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+const { Application } = Ember;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-const app = Ember.Application.extend({
+const app = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
