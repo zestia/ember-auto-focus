8323186130c4684853d98aff825acced75c5ee84
Andrew Kirwin <andrew.kirwin@gmail.com>
2019-08-17 17:18:39 +0100

    run codemod

--- tests/integration/components/auto-focus-test.js
+++ tests/integration/components/auto-focus-test.js
@@ -1,6 +1,12 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, settled, waitUntil, getSettledState, find } from '@ember/test-helpers';
+import {
+  render,
+  settled,
+  waitUntil,
+  getSettledState,
+  find
+} from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('auto-focus', function(hooks) {
