7c14b528f8bba6013d67aaf29b6c63a6b89a110c
Andrew Kirwin <andrew.kirwin@zestia.com>
2020-06-22 14:56:23 +0100

    always a string anyway...

--- addon/utils/focus.js
+++ addon/utils/focus.js
@@ -1,7 +1,7 @@
 import { next } from '@ember/runloop';
 
 export default function focus(element) {
-  element.dataset.programmaticallyFocused = true;
+  element.dataset.programmaticallyFocused = 'true';
   element.focus();
   next(() => delete element.dataset.programmaticallyFocused);
 }
